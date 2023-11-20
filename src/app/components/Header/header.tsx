1e7675f588bd7a100344839fbae36c2182ae62ae
曹操 <xuzhao.sgn@gmail.com>
2023-11-20 21:39:09 +0800

    add hero section

--- src/app/components/Header/header.tsx
+++ src/app/components/Header/header.tsx
@@ -1,11 +1,9 @@
 'use client'
 
 import * as React from 'react'
-import { Logo } from '../icon'
 import Link from 'next/link'
-import { ActiveLink,MobileMenu } from './menu'
+import { LogoIcon } from '../Common/icon'
 import { Disclosure } from '@headlessui/react'
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 
 
 function classNames(...classes: string[]) {
@@ -16,66 +14,64 @@ export default function Header() {
 
   return(
   
-    <div className='max-w-7xl mx-auto px-4 md:px-12 py-6 min-h-full'>
+    <div className='px-4 md:px-12 py-6 min-h-full'>
       <Disclosure as="nav" className="">
       {({ open }) => (
       <>
-       <div className='flex items-center justify-center space-x-6 cursor-pointer'>
+       <div className='flex items-center justify-center space-x-3 cursor-pointer'>
          <Link href='/'>
           <div className='w-6'>
-            <Logo/>
+            <LogoIcon/>
           </div>
         </Link>
         <Link href='/'>
-          <div className='text-2xl text-[#222C3C] font-bold font-brand'>
-            Shirley
+          <div className='text-2xl text-[#222C3C] font-brand'>
+            Estack
           </div>
         </Link>
         <div className='grow'>
         </div>
         <nav>
-          <div className='md:flex items-center justify-center space-x-10 hidden'>
-            <ActiveLink href='/'>
-              <div className='text-lg'>
-                Home
+          <div className='md:flex items-center justify-center space-x-12 hidden text-[#222C3C]'>
+            <Link href='https://docs.estackdao.com' target='_blank'>
+              <div className='text-lg font-text font-medium'>
+                Docs
               </div>
-            </ActiveLink>
-            <ActiveLink href='/work'>
-              <div className='text-lg'>
-                Work
+            </Link>
+            <Link href='https://github.com/estackdao' target='_blank'>
+              <div className='text-lg font-text font-medium'>
+                Github
               </div>
-            </ActiveLink>
-            <ActiveLink href='/about'>
-              <div className='text-lg'>
-                About
+            </Link>
+            <Link href='https://twitter.com/estackdao' target='_blank'>
+              <div className='text-lg font-text font-medium'>
+                Twitter
               </div>
-            </ActiveLink>
+            </Link>
           </div>
         </nav>
         
-        <Link href='mailto:duanmengq@gmail.com'>
-          <div className='rounded-3xl bg-[#222C3C] px-2 md:px-4 py-2 text-base text-white'>
-            Let's Connect
-          </div>
-        </Link>        
-        
+        <div className='pl-10'>
+          <Link href='mailto:hello@estackdao.com'>
+            <div className='rounded-3xl bg-[#222C3C] px-2 md:px-4 py-2 text-base text-white'>
+              Contact Us
+            </div>
+          </Link>    
+        </div>
 
         <div className="flex md:hidden">
         {/* Mobile menu button */}
           <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md border-[1px] border-[#222C3C] bg-white p-1">
             <span className="absolute -inset-0.5" />
             <span className="sr-only">Open main menu</span>
-            {open ? (
+            {/* {open ? (
                 <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
             ) : (
                 <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-            )}
+            )} */}
           </Disclosure.Button>
         </div>
       </div>
-
-      <MobileMenu/>
-
 
       </>)}
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily:{
+        brand: ['Zen Dots'],
+        text:['Roboto'],
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
